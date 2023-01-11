36bc24cef28e0fba4418a0ccef3a8629de1ba46a
VladPryima <114105594+VladPryima@users.noreply.github.com>
2023-01-11 15:42:33 +0200

    add comments

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Collection } from './Collection';
 import './index.scss';
 
+//Категорії
 const cats = [
   { "name": "Усі" },
   { "name": "Авто" },
@@ -14,19 +15,21 @@ const cats = [
 function App() {
   const [categoryId, setCategoryId] = React.useState(0);
   const [page, setPage] = React.useState(1);
-  const [searchValue, setSearchValue] = React.useState("")
-  const [collections, setCollections] = React.useState([]);
+  const [searchValue, setSearchValue] = React.useState(""); //Стейт для пошуку
+  const [collections, setCollections] = React.useState([]); //Дані з JSON
   React.useEffect(() => {
-    const category = categoryId ? `category=${categoryId}` : '';
+   const category = categoryId ? `category=${categoryId}` : '';
     
-
-    fetch(`https://639d0fe942e3ad6927408652.mockapi.io/photo_collections?page=${page}&limit=3&${category}`)
+//`https://639d0fe942e3ad6927408652.mockapi.io/photo_collections?page=${page}&limit=3&${category}`
+//Отримуємо JSON з Mock API
+    fetch(`https://63be8495e348cb076210bb5f.mockapi.io/photo?page=${page}&limit=3&${category}`)
     .then((res) => res.json())
     .then(json => {
       setCollections(json);
     });
     
   }, [categoryId, page]);
+
 
   return (
     
@@ -35,15 +38,18 @@ function App() {
       <h1>Коллекція артів з <span className="spanDribble">Dribble</span></h1>
       <div className="top">
         <ul className="tags">
-          
+          {/*Створюємо категорії*/}
           {
             cats.map((obj, i) => (
             <li onClick={() => setCategoryId(i)} className={categoryId === i ? "active": ""} key={obj.name}>{obj.name}</li>))
           }
         </ul>
+        {/*Пошук*/}
         <input value={searchValue} onChange={e => setSearchValue(e.target.value)} className="search-input" placeholder="Пошук за назвою" />
       </div>
+
       <div className="content">
+        {/*Фільтруємо елементи на сторінці - повертаємо компонент Collection, фільтруючи данні по searchValue*/}
         {collections.filter(obj => {
           return obj.name.toLowerCase().includes(searchValue.toLocaleLowerCase())
         })
@@ -53,6 +59,7 @@ function App() {
       </div>
       <ul className="pagination">
         {
+          
           [...Array(5)].map((_,i) => (
              <li onClick={() => setPage(i + 1)} className={page === i + 1 ? 'active' : ''}>{i + 1}</li>
              ))
